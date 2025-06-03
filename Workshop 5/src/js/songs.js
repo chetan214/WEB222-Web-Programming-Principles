6415db5f4cfc417d9314424ec75f2378c3d00a93
Chetan Arora <159317070+chetan214@users.noreply.github.com>
2025-06-03 14:43:20 -0400

    Add files via upload

---
+++ Workshop 5/src/js/songs.js
@@ -0,0 +1,85 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  /* TODO */
+  //KK
+  {
+    songId: "SID-0001",
+    artistId: "AID-0001",
+    title: "Dil Ibaadat",
+    year: "2008",
+    duration: 232,
+    url: "https://youtu.be/BAeGLiBZuiQ?si=dSk0dAJurlRwKZ651",
+    explicit: false,
+    imageUrl: "dilibaadat.jpg"
+  },
+  {
+    songId: "SID-0002",
+    artistId: "AID-0001",
+    title: "Zara Sa",
+    year: "2008",
+    duration: 228,
+    url: "https://youtu.be/lUPltG1hb3k?si=zhTFbQvxWE-8ywEw",
+    explicit: false,
+    imageUrl: "zarasa.jpg"
+  },
+  //Atif Aslam
+  {
+    songId: "SID-0003",
+    artistId: "AID-0002",
+    title: "Aadat (Juda Hoke Bhi)",
+    year: "2005",
+    duration: 290,
+    url: "https://youtu.be/e5LShHAE03A?si=_as8Sg_HoQH5scEV",
+    explicit: true,
+    imageUrl: "aadat.jpg"
+  },
+  {
+    songId: "SID-0004",
+    artistId: "AID-0002",
+    title: "Khair Mangda",
+    year: "2017",
+    duration: 265,
+    url: "https://youtu.be/Sw5JrUc-rFI?si=s46ubp2o2tL0ssgd",
+    explicit: false,
+    imageUrl: "khair.jpg"
+  },
+  //Arijit Singh
+  {
+    songId: "SID-0005",
+    artistId: "AID-0003",
+    title: "Tum Hi Ho",
+    year: "2013",
+    duration: 267,
+    url: "https://youtu.be/Umqb9KENgmk?si=a_0oCpUpVq_rBNjW",
+    explicit: false,
+    imageUrl: "tumhiho.jpg"
+  },
+
+  {
+    songId: "SID-0006",
+    artistId: "AID-0003",
+    title: "Channa Mereya",
+    year: "2016",
+    duration: 346,
+    url: "https://youtu.be/bzSTpdcs-EI?si=AwY16dAlVeQY_OBO",
+    explicit: false,
+    imageUrl: "channa.jpg"
+  },
+  //AR Rahman
+  {
+    songId: "SID-0007",
+    artistId: "AID-0004",
+    title: "Jai Ho",
+    year: "2008",
+    duration: 179,
+    url: "https://youtu.be/xwwAVRyNmgQ?si=bMztlCwf3T911rc6",
+    explicit: false,
+    imageUrl: "jaiho.jpg"
+  },
+  
+];
---
+++ Workshop 5/src/public/js/artists.js
@@ -0,0 +1,54 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  /* TODO */
+  {
+    artistId: "AID-0001",
+    name: "Krishnakumar Kunnath(KK)",
+    urls: [
+      {
+        url: "https://open.spotify.com/artist/4fEkbug6kZzzJ8eYX6Kbbp",
+        name: "Spotify"
+      },
+      { url: "https://www.instagram.com/kk_live_now/?hl=en", name: "Instagram" }
+    ],
+    artistImageUrl: "kk.jpg"
+  },
+  {
+    artistId: "AID-0002",
+    name: "Atif Aslam",
+    urls: [
+      { url: "https://www.youtube.com/channel/UCF4uIIqbIy05Cmzx3rRt_8g", name: "YouTube" },
+      {
+        url: "https://open.spotify.com/artist/2oSONSC9zQ4UonDKnLqksx",
+        name: "Spotify"
+      }
+    ],
+    artistImageUrl: "atif.jpg"
+  },
+  {
+    artistId: "AID-0003",
+    name: "Arijit Singh",
+    urls: [
+      {
+        url: "https://open.spotify.com/artist/4YRxDV8wJFPHPTeXepOstw",
+        name: "Spotify"
+      },
+      { url: "https://www.youtube.com/watch?v=_z8UyrLlM7k", name: "YouTube" }
+    ],
+    artistImageUrl: "arijit.jpg"
+  },
+  {
+    artistId: "AID-0004",
+    name: "A. R. Rahman",
+    urls: [
+      { url: "https://www.instagram.com/arrahman/?hl=en", name: "Instagram" },
+      { url: "https://www.youtube.com/channel/UC3mb5QRlm4VQmOZD_P0ctGw", name: "YouTube" }
+    ],
+    artistImageUrl: "ar.jpg"
+  }
+];
